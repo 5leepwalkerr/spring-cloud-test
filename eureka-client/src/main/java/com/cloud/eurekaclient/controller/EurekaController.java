0128ab20b0741baedce651e03a391ad1080d060e
apachemax <cvb.cvb.2017.cvb@gmail.com>
2023-06-28 18:30:40 +0300

    add eureka client

---
+++ eureka-client/src/main/java/com/cloud/eurekaclient/controller/EurekaController.java
@@ -0,0 +1,16 @@
+package com.cloud.eurekaclient.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class EurekaController {
+
+    @GetMapping()
+    public String test(){
+        return "test";
+    }
+
+}
